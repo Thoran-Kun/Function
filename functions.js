7e6b0e8ca3cf4d4eff6bdaa328b38a37ad1e5a83
Salvatore <pepesalvatore12@gmail.com>
2025-10-02 16:59:20 +0200

    end function

---
+++ functions.js
@@ -0,0 +1,141 @@
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const area = function (l1, l2) {
+  let result = l1 * l2
+  console.log(result)
+}
+
+area(15, 20)
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const crazySum = function (n1, n2) {
+  let somma = n1 + n2
+  if (n1 !== n2) {
+    return true
+  } else {
+    return somma * 3
+  }
+}
+console.log(crazySum(5, 4))
+
+crazySum(5, 4)
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const crazyDiff = function (n1, n2) {
+  let differenza = n1 - n2
+  if (n1 > n2) {
+    return differenza * 3
+  } else {
+    return differenza
+  }
+}
+console.log(crazyDiff(34, 19))
+
+crazyDiff(34, 19)
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const boundary = function (n1, n2 = 400) {
+  let num = n1
+  if (n1 > 20 && n1 < 101) {
+    return true
+  } else {
+    return (n1 = 400)
+  }
+}
+console.log(boundary(34))
+
+boundary(34)
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+// /* SCRIVI QUI LA TUA RISPOSTA */
+// const epify = function () {
+//     let message =
+// }
+// epify("CIAO, ")
+// epify("CIAO, EPICODE")
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const check3and7 = function (n1) {
+  let num = n1
+  if (n1 % 3 === 0 || n1 % 7 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+console.log(check3and7(9))
+
+check3and7(9)
+
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const reverseString = function () {
+  let message = "EPICODE"
+  let newMessage = message.replace("EPICODE", "EDOCIPE")
+  console.log(newMessage)
+}
+reverseString()
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const upperFirst = function () {
+  let frase = "sono un nuovo messaggio di testo"
+}
+upperFirst()
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const cutString = function () {
+  let message = "sono un nuovo messaggio di testo"
+  let lungMessage = message.length
+  console.log(lungMessage)
+  lungMessage = message.slice(1, 31)
+  console.log(lungMessage)
+}
+cutString()
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
